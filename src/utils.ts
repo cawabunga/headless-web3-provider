d957d066f19692c0094a238de1c9b1fddc68b7f2
Emil Ibatullin <emil.ibatullin@gmail.com>
2022-11-25 11:49:47 +0300

    Fix SendTransaction method name

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export enum Web3RequestKind {
   RequestAccounts = 'eth_requestAccounts',
   Accounts = 'eth_accounts',
-  SendTransaction = 'eth_accounts',
+  SendTransaction = 'eth_sendTransaction',
   SwitchEthereumChain = 'wallet_switchEthereumChain',
   AddEthereumChain = 'wallet_addEthereumChain',
 }
