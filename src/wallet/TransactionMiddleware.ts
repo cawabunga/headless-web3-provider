fa4b5da03ed4eb17adfd574d18ba71acea1b9874
v1rtl <hi@v1rtl.site>
2024-07-29 20:10:35 +0300

    cap gas to ensure it's always above 21K

--- src/wallet/TransactionMiddleware.ts
+++ src/wallet/TransactionMiddleware.ts
@@ -9,6 +9,7 @@ import {
 	type Hex,
 	type LocalAccount,
 	type TransactionRequest,
+	createPublicClient,
 	createWalletClient,
 	hexToBigInt,
 } from 'viem'
@@ -64,6 +65,15 @@ export function createTransactionMiddleware({
 	account: LocalAccount
 	getChainTransport: () => ChainTransport
 }) {
+	const walletClient = createWalletClient({
+		account,
+		chain: getChain(),
+		transport: getChainTransport,
+	})
+	const publicClient = createPublicClient({
+		chain: getChain(),
+		transport: getChainTransport,
+	})
 	const middleware: JsonRpcMiddleware<JsonRpcParams, Json> =
 		createAsyncMiddleware(async (req, res, next) => {
 			switch (req.method) {
@@ -71,20 +81,30 @@ export function createTransactionMiddleware({
 					// @ts-expect-error todo: parse params
 					const jsonRpcTx = req.params[0] as JsonRpcTx
 
-					let viemTx!: TransactionRequest
+					let viemTx: TransactionRequest
 					try {
 						viemTx = toViemTx(jsonRpcTx)
 					} catch (e) {
 						console.error('Error parsing tx', e)
+						throw e
 					}
 
-					const tx = await createWalletClient({
-						account,
-						chain: getChain(),
-						transport: getChainTransport,
-					}).sendTransaction(viemTx)
+					try {
+						const estimatedGasLimit = await publicClient.estimateGas(viemTx)
 
-					res.result = tx
+						res.result = await walletClient.sendTransaction({
+							...viemTx,
+							// I'm not sure why a submitted tx gas has a gas value below 21K so we cap it here
+							gas: viemTx.gas
+								? estimatedGasLimit > viemTx.gas
+									? estimatedGasLimit
+									: viemTx.gas
+								: undefined,
+						})
+					} catch (e) {
+						console.error('Error submitting transaction', e, viemTx)
+					}
+
 					break
 				}
 
