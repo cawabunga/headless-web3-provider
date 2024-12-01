8da89c8973d032ecb7c73ac3a48495fae5aa3088
Emil Ibatullin <cawabunga@users.noreply.github.com>
2024-12-01 12:08:07 +0100

    Refactor EIP-6963 implementation (#26)

--- src/playwright.ts
+++ src/playwright.ts
@@ -2,6 +2,11 @@ import type { Page } from '@playwright/test'
 import { Web3ProviderConfig } from './Web3ProviderBackend'
 import { makeHeadlessWeb3Provider } from './factory'
 import { IWeb3Provider } from './types'
+import {
+  EIP6963_EVENTS,
+  EIP6963_PROVIDER_INFO,
+  EIP6963ProviderDetail,
+} from './EIP6963'
 
 type Fn = (...args: any[]) => any
 
@@ -54,10 +59,8 @@ export async function injectHeadlessWeb3Provider(
       web3Provider[method](...args)
   )
 
-  const uuid = 'b9838e9f-e9bc-48dd-af0b-6f98949ae677'
-
   await page.addInitScript(
-    (uuid) => {
+    ([EIP6963_EVENTS, EIP6963_PROVIDER_INFO]) => {
       class EventEmitter {
         private readonly listeners: Record<
           string,
@@ -116,28 +119,29 @@ export async function injectHeadlessWeb3Provider(
           return Reflect.get(...arguments)
         },
       })
+
+      // This is not part of the EIP-1193 or EIP-6963 standards
+      // https://github.com/wevm/references/issues/167
       window.dispatchEvent(new Event('ethereum#initialized'))
 
-      const event = new CustomEvent('eip6963:announceProvider', {
-        detail: Object.freeze({
-          info: {
-            icon: "data:image/svg+xml,%3Csvg height='99px' width='99px' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 115 182'%3E%3Cpath d='M57.5054 181V135.84L1.64064 103.171L57.5054 181Z' fill='%23F0CDC2' stroke='%231616B4' stroke-linejoin='round'/%3E%3Cpath d='M57.6906 181V135.84L113.555 103.171L57.6906 181Z' fill='%23C9B3F5' stroke='%231616B4' stroke-linejoin='round'/%3E%3Cpath d='M57.5055 124.615V66.9786L1 92.2811L57.5055 124.615Z' fill='%2388AAF1' stroke='%231616B4' stroke-linejoin='round'/%3E%3Cpath d='M57.6903 124.615V66.9786L114.196 92.2811L57.6903 124.615Z' fill='%23C9B3F5' stroke='%231616B4' stroke-linejoin='round'/%3E%3Cpath d='M1.00006 92.2811L57.5054 1V66.9786L1.00006 92.2811Z' fill='%23F0CDC2' stroke='%231616B4' stroke-linejoin='round'/%3E%3Cpath d='M114.196 92.2811L57.6906 1V66.9786L114.196 92.2811Z' fill='%23B8FAF6' stroke='%231616B4' stroke-linejoin='round'/%3E%3C/svg%3E",
-            name: 'Headless Web3 Provider',
-            rdns: 'headless-web3-provider',
-            uuid: uuid,
-          },
-          provider: window.ethereum,
-        }),
+      const providerDetail: EIP6963ProviderDetail = Object.freeze({
+        info: EIP6963_PROVIDER_INFO,
+        provider: window.ethereum,
       })
 
-      window.dispatchEvent(event)
+      // https://eips.ethereum.org/EIPS/eip-6963#announce-and-request-events
+      const announceEvent = new CustomEvent(EIP6963_EVENTS.ANNOUNCE_PROVIDER, {
+        detail: providerDetail,
+      })
+      window.dispatchEvent(announceEvent)
 
-      const handler = () => window.dispatchEvent(event)
-      window.addEventListener('eip6963:requestProvider', handler)
-      return () =>
-        window.removeEventListener('eip6963:requestProvider', handler)
+      const handler = () => window.dispatchEvent(announceEvent)
+      window.addEventListener(EIP6963_EVENTS.REQUEST_PROVIDER, handler)
+      return () => {
+        window.removeEventListener(EIP6963_EVENTS.REQUEST_PROVIDER, handler)
+      }
     },
-    [uuid]
+    [EIP6963_EVENTS, EIP6963_PROVIDER_INFO] as const
   )
 
   return web3Provider
